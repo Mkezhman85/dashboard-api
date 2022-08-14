d508d66f4e22d0b4fa3048ec1619fa57e06d71ba
Sergei <34881448+Mkezhman85@users.noreply.github.com>
2022-08-14 10:40:21 +0300

    Реализация зависимостей с помощью inversify, reflect-metadata

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,38 +1,40 @@
 import { Response, Router } from 'express';
+import { injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
 import { LoggerService } from '../logger/logger.service';
 import { IControllerRoute } from './route.interface';
+import 'reflect-metadata';
 
+@injectable()
 export abstract class BaseController {
-
 	private readonly _router: Router;
 
-	constructor(private logger: LoggerService) {
+	constructor(private logger: ILogger) {
 		this._router = Router();
 	}
 
-	get router() {
+	get router(): Router {
 		return this._router;
 	}
 
-	public send<T>(res: Response, code: number, message: T) {
+	public send<T>(res: Response, code: number, message: T): Response {
 		res.type('application/json');
 		return res.sendStatus(200).json();
 	}
 
-	public ok<T>(res: Response, message: T) {
+	public ok<T>(res: Response, message: T): Response {
 		return this.send(res, 200, message);
 	}
 
-	public created(res: Response) {
+	public created(res: Response): Response {
 		return res.sendStatus(201);
 	}
 
-	protected bindRoutes(routes: IControllerRoute[]) {
+	protected bindRoutes(routes: IControllerRoute[]): void {
 		for (const route of routes) {
 			this.logger.log(`[${route.method} ${route.path}]`);
 			const handler = route.func.bind(this);
 			this.router[route.method](route.path, handler);
 		}
-		
 	}
 }
