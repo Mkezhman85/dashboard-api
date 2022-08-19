8111b87e3a7be83b96053ccf1cc653229648a067
Sergei <34881448+Mkezhman85@users.noreply.github.com>
2022-08-19 12:08:06 +0300

    Add users repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,13 +26,26 @@ export class UserController extends BaseController implements IUserController {
 				func: this.register,
 				middlewares: [new ValidateMiddleware(UserRegisterDto)],
 			},
-			{ path: '/login', method: 'post', func: this.login },
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
 		]);
 	}
 
-	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
-		next(new HTTPError(401, 'Ошибка авторизации', 'login'));
+	async login(
+		req: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.validateUser(req.body);
+		if (!result) {
+			return next(new HTTPError(401, 'Ошибка авторизации', 'login'));
+		}
+		this.ok(res, {});
+		this.loggerService.log(`[UserService] Авторизация пользователя`);
 	}
 
 	async register(
@@ -44,7 +57,7 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(422, 'Такой пользователь уже существует'));
 		}
-		this.ok(res, { email: result.email });
-		this.loggerService.log(`Создан пользователь ${result}`);
+		this.ok(res, { email: result.email, id: result.id });
+		this.loggerService.log(`[UserService] Создан пользователь. ${result.email}`);
 	}
 }
